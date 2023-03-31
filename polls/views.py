4ee4570386ea3ee5a9e5bad99f43aeea8a63dc9b
antikkorps <franckvienot7@gmail.com>
2023-03-31 17:40:32 +0200

    unit tests

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -17,8 +18,8 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
